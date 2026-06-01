3163192fd5ddd396f02777759a1781f89f80adf7
aron11114 <arongc1114@gmail.com>
2026-05-31 21:23:33 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11083691
+    "domain": "5gvci.com",
+    "zoneId": 11084105
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
